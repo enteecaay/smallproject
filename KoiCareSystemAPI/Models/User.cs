3f8ae740bd7765137b44c48b374d7a19a221381c
layze <layzegot@gmail.com>
2024-09-09 08:30:12 +0700

    revert

--- KoiCareSystemAPI/Models/User.cs
+++
@@ -1,8 +0,0 @@
-public class User
-{
-    public int UserID { get; set; }
-    public string Username { get; set; }
-    public string PasswordHash { get; set; }
-    public string Email { get; set; }
-    public DateTime CreatedAt { get; set; }
-}
